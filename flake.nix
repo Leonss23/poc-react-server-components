0b7e123433c954bb43528912ef88c30b00ed128b
Leonardo Gatti <leonardogattidev@gmail.com>
2023-12-20 05:50:55 -0300

    init

---
+++ flake.nix
@@ -0,0 +1,19 @@
+{
+    description= "React web components implementation POC";
+
+    inputs = {
+        nixpkgs.url = "github:NixOS/nixpkgs";
+    };
+
+    outputs = {nixpkgs, ...}:
+    let
+        system = "x86_64-linux";
+        pkgs = nixpkgs.legacyPackages.${system};
+    in {
+        devShells.${system}.default = pkgs.mkShell {
+            buildInputs = with pkgs; [
+                bun
+            ];
+        };
+    };
+}
